fce79f0325d76fe2fae6478421f8026d6b012e98
CJ Davis <csdj92@gmail.com>
2020-09-08 19:46:33 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 def speak_to_grandma(talk)
   if talk !=talk.upcase
     return "HUH?! SPEAK UP, SONNY!"
-    elsif talk==talk.upcase
+    elsif talk==talk.upcase&& talk!= "I LOVE YOU GRANDMA!"
     return "NO, NOT SINCE 1938!"
   if talk== "I LOVE YOU GRANDMA"
     return "I LOVE YOU TOO PUMPKIN"
